4186bba9be2b14bc5d97d099a3ca3f2d4f23d015
ajtollo <khesir2002@gmail.com>
2024-02-01 17:50:29 +0800

    third commit

---
+++ src/components/bigLeftContainer/LeftContainer.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function LeftContainer(){
+    return(
+        <div className="p-10 bg-slate-500">
+            <div className="">
+                
+            </div>
+        </div>
+    );
+}
---
+++ src/components/header/SimpleHeader.tsx
@@ -0,0 +1,8 @@
+
+export function SimpleHeader() {
+    return(
+        <div className="flex bg-slate-500 p-5">
+            <h1 className=" font-extrabold">Pokedex</h1>
+        </div>
+    );
+}
